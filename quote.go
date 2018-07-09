0d003b9c4bfac881641be8eb1598b782a467a97f
Russ Cox <rsc@golang.org>
2018-07-09 12:03:52 -0400

    quote: update v1 to use v2

--- quote.go
+++ quote.go
@@ -5,26 +5,26 @@
 // Package quote collects pithy sayings.
 package quote // import "rsc.io/quote"
 
-import "rsc.io/sampler"
+import "rsc.io/quote/v2"
 
 // Hello returns a greeting.
 func Hello() string {
-	return sampler.Hello()
+	return quote.HelloV2()
 }
 
 // Glass returns a useful phrase for world travelers.
 func Glass() string {
 	// See http://www.oocities.org/nodotus/hbglass.html.
-	return "I can eat glass and it doesn't hurt me."
+	return quote.GlassV2()
 }
 
 // Go returns a Go proverb.
 func Go() string {
-	return "Don't communicate by sharing memory, share memory by communicating."
+	return quote.GoV2()
 }
 
 // Opt returns an optimization truth.
 func Opt() string {
 	// Wisdom from ken.
-	return "If a program is too slow, it must have a loop."
+	return quote.OptV2()
 }
