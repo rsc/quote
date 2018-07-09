fe488b867524806e861c3f4f43ae6946a42ca3f1
Russ Cox <rsc@golang.org>
2018-07-09 12:28:16 -0400

    oops! break v3 subdirectory

--- v3/quote.go
+++ v3/quote.go
@@ -10,21 +10,3 @@ import "rsc.io/sampler"
 // Hello returns a greeting.
 func HelloV3() string {
 	return sampler.Hello()
-}
-
-// Glass returns a useful phrase for world travelers.
-func GlassV3() string {
-	// See http://www.oocities.org/nodotus/hbglass.html.
-	return "I can eat glass and it doesn't hurt me."
-}
-
-// Go returns a Go proverb.
-func GoV3() string {
-	return "Don't communicate by sharing memory, share memory by communicating."
-}
-
-// Opt returns an optimization truth.
-func OptV3() string {
-	// Wisdom from ken.
-	return "If a program is too slow, it must have a loop."
-}
