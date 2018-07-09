754f68430672776c84704e2d10209a6ec700cd64
Russ Cox <rsc@golang.org>
2018-07-09 11:32:44 -0400

    quote: change to V2 API

--- quote.go
+++ quote.go
@@ -8,23 +8,23 @@ package quote // import "rsc.io/quote"
 import "rsc.io/sampler"
 
 // Hello returns a greeting.
-func Hello() string {
+func HelloV2() string {
 	return sampler.Hello()
 }
 
 // Glass returns a useful phrase for world travelers.
-func Glass() string {
+func GlassV2() string {
 	// See http://www.oocities.org/nodotus/hbglass.html.
 	return "I can eat glass and it doesn't hurt me."
 }
 
 // Go returns a Go proverb.
-func Go() string {
+func GoV2() string {
 	return "Don't communicate by sharing memory, share memory by communicating."
 }
 
 // Opt returns an optimization truth.
-func Opt() string {
+func OptV2() string {
 	// Wisdom from ken.
 	return "If a program is too slow, it must have a loop."
 }
